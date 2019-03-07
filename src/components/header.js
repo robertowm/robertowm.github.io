0c26213430cbd26d625de8a3400cb26a67e6b8fd
Roberto Weidmann Menezes <robertowm@gmail.com>
2019-03-07 00:33:07 -0300

    Improve header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,36 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = ({ siteTitle }) => {
+  const navbarStyle = {
+    background: 'rebeccapurple',
+    marginBottom: '1.45rem',
+  }
+
+  const innerNavbarStyle = {
+    margin: '0 auto',
+    maxWidth: 960,
+    padding: '1.45rem 1.0875rem',
+  }
+
+  const titleStyle = { margin: 0 }
+
+  const linkStyle = {
+    color: 'white',
+    textDecoration: 'none',
+  }
+
+  return (
+    <div style={navbarStyle}>
+      <div style={innerNavbarStyle}>
+        <h1 style={titleStyle}>
+          <Link to="/" style={linkStyle}>
+            {siteTitle}
+          </Link>
+        </h1>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Header
