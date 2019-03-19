06b4e07318ee7e1f27279e61e1356ecf1cbbf894
Roberto Weidmann Menezes <robertowm@gmail.com>
2019-03-19 04:30:15 -0300

    Update index.js and related files

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,20 +9,30 @@ import 'bootstrap/dist/css/bootstrap.css'
 export const query = graphql`
 query {
   contentYaml {
-    title,
-    body
+    summary,
+    address,
+    email,
+    profile_url,
+    cv_url,
+    photo,
+    links {
+      name
+      url
+    }
   }
 }
 `;
 
-const IndexPage = ({data: { contentYaml:content }}) => (
-  <Layout>
-    <h1>{content.title}</h1>
-    <div class="content">
-      {content.body}
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({ data: { contentYaml: content } }) => {
+  const summaryEntries = content.summary.split('\n').map(summary => (<p>{summary}</p>));
+  return (
+    <Layout>
+      <div class="summary">
+        {summaryEntries}
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  );
+};
 
 export default IndexPage
