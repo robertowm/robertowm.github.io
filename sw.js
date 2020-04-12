ba678599ca255cad84a2121b58ef3db9ae8663cf
Roberto Menezes <robertowm@gmail.com>
2020-04-12 06:27:59 +0000

    Update master with new version [skip ci]

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d26adc7c80f38847e268.js"
+    "url": "webpack-runtime-37fba723117180e6b9a1.js"
   },
   {
     "url": "framework-7656862d80676d58607a.js"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0db5e819a46129acd15a6c78e63d80e1"
+    "revision": "4ccf2c222caffc2461ef4d284656dd7f"
   },
   {
     "url": "manifest.webmanifest",
